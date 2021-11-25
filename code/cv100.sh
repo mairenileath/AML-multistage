9c3360f616898cc82d25efd366b231f76b0b4543
mairenileath <61795083+mairenileath@users.noreply.github.com>
2021-11-25 19:55:49 +0000

    Delete code directory

--- code/cv100.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-#BSUB -J cv100[1-100]
-#BSUB -o logs/cv100.out%J
-#BSUB -e logs/cv100.err%J
-#BSUB -M 1000
-#BSUB -R "select[mem>1000] rusage[mem=1000]" 
-R --no-save < cv100.R
--- code/imputation.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-#BSUB -J imputation[1-1540]
-#BSUB -o logs/imputation.out%J
-#BSUB -e logs/imputation.err%J
-#BSUB -M 1000
-#BSUB -R "select[mem>1000] rusage[mem=1000]" 
-R --no-save < imputation.R
